38172b136679809c8114bdbf60de8398b353dc4d
Esteem Wilson <vvtrong203@gmail.com>
2025-03-08 22:01:18 +0700

    okkk

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,10 @@ const CONFIGDATA = {
   buttonNo: "Để sau",
   titleModar: "Chúc mừng ngày 8/3 Mẹ yêu!",
   descriptionModar:
-      "Con cảm ơn mẹ vì tất cả những gì mẹ đã làm cho con và gia đình mình. </br> Mẹ là người phụ nữ mạnh mẽ nhất, người đã luôn hy sinh và yêu thương con vô điều kiện. </br> Con chúc mẹ luôn khỏe mạnh, hạnh phúc và xinh đẹp mỗi ngày. </br> Tối nay con sẽ chuẩn bị một bữa tối đặc biệt cho mẹ. Mẹ cứ nghỉ ngơi để con lo hết nhé! </br> Con yêu Mẹ rất nhiều <3",
+      "Con cảm ơn mẹ vì tất cả những gì mẹ đã làm cho con và gia đình mình. </br> Mẹ là người phụ nữ mạnh mẽ nhất, người đã luôn hy sinh và yêu thương con vô điều kiện. </br> Con chúc mẹ luôn khỏe mạnh, hạnh phúc và xinh đẹp mỗi ngày. </br> " +
+
+      "Chúc mừng ngày Quốc tế Phụ nữ 8/3, mẹ yêu! Cảm ơn mẹ đã luôn là người phụ nữ tuyệt vời nhất trong cuộc đời con. Con yêu mẹ rất nhiều!\n"
++
+      "</br>Con yêu Mẹ rất nhiều <3 😘😘😘😘😘😘"
+
 };
